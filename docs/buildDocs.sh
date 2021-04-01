33cc4dab3a9728f1a82ca0f48a27d487dce75653
Brendon Duncan <brendonmduncan@gmail.com>
2021-04-01 15:51:02 +1000

    Update buildDocs.sh

--- docs/buildDocs.sh
+++ docs/buildDocs.sh
@@ -49,10 +49,7 @@ pushd "${docroot}"
 # don't bother maintaining history; just generate fresh
 git init
 git remote add deploy "https://token:${GITHUB_TOKEN}@github.com/${GITHUB_REPOSITORY}.git"
-git checkout -b gh-pages
-
-# Branch into gh-pages-pull
-git branch gh-pages-pull
+git checkout -b gh-pages-pull
  
 # add .nojekyll to the root so that github won't 404 on content added to dirs
 # that start with an underscore (_), such as our "_content" dir..
