5da14cd8544f6057e2a07b371132779ad7f7ed60
Brendon Duncan <brendonmduncan@gmail.com>
2021-04-01 22:03:21 +1000

    Added a lot to the Configuration section of the disco_l475_iot1 SPI, can't be fucked at the moment

--- docs/conf.py
+++ docs/conf.py
@@ -62,5 +62,9 @@ html_theme = 'sphinx_rtd_theme'
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ['_static']
 
+html_css_files = [
+    'css/blue_highlight.css'
+]
+
 master_doc = 'index'
 html_show_sourcelink = False
